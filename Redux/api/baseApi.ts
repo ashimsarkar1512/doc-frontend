c0a186e990dad737e2937e83046a18ebfad88c5f
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-06-08 15:56:57 +0600

    complete the contact section button

--- Redux/api/baseApi.ts
+++ Redux/api/baseApi.ts
@@ -46,7 +46,7 @@ const baseQueryWithReauth: BaseQueryFn<
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: baseQueryWithReauth,
-  tagTypes: ['Auth', 'User', 'Chat', 'Message', 'Consultation'],
+  tagTypes: ['Auth', 'User', 'Chat', 'Message', 'Consultation', 'ContactLead'],
   endpoints: () => ({}),
 })
 
