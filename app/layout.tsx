680a9b9495330461ecb1f5db1a69387f27eac8ab
devlopersabbir <devlopersabbir@gmail.com>
2026-06-28 16:52:40 +0600

    chore: clean up console logs in layout and update bun lockfile dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,13 +31,6 @@ const quicksand = Quicksand({
 // ✅ static metadata REMOVE করো, এটা বসাও
 export async function generateMetadata(): Promise<Metadata> {
   const seo = await getSeoData();
-<<<<<<< HEAD
-<<<<<<< HEAD
-  console.log("SEO DATA check:", seo?.title, seo?.faviconLight?.fileUrl);
-  console.log("SEO DATA check:", seo?.metaDescription);
-=======
-=======
->>>>>>> fd37be0c78e24c7657761c90725b31a13d00e734
 
   if (!seo) {
     console.log("waiting for load data");
@@ -46,10 +39,6 @@ export async function generateMetadata(): Promise<Metadata> {
       description: "Medical Weight Management Program",
     };
   }
-<<<<<<< HEAD
->>>>>>> dev
-=======
->>>>>>> fd37be0c78e24c7657761c90725b31a13d00e734
 
   const faviconLightUrl = seo?.faviconLight?.fileUrl || "/favicon.ico";
   const faviconDarkUrl = seo?.faviconDark?.fileUrl || "/favicon.ico";
