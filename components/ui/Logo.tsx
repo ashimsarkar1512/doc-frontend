9c1a3359fd054907abdb4a78773f99d279f140c8
unknown <saifulislamshaikot@softvence.com>
2026-07-02 19:28:15 +0600

    fixed

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // import { useGetWebsiteSettingsQuery } from "@/Redux/features/footerData/footerDataApi";
 // import Image from "next/image";
 // import React from "react";
