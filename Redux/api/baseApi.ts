44a520d09a1a856345addbb501ebd3f3218fd5c2
imransoftvence <imranmia@softvence.com>
2026-07-04 17:30:45 +0600

    finish

--- Redux/api/baseApi.ts
+++ Redux/api/baseApi.ts
@@ -55,7 +55,8 @@ const baseQueryWithReauth: BaseQueryFn<
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: baseQueryWithReauth,
-  tagTypes: ['Auth', 'User', 'Chat', 'Message', 'Consultation', 'ContactLead','Categories','Products','DoctorDashboard','Consultations','Patient', 'Dashboard','Orders','Cart','HomepageContent','WebsiteSettings','Notifications','Doctors', 'Testimonials', 'Discounts', 'HeroSections', 'CtaSections', 'CoverageSection', 'Faq', 'Eligibility'],
+  tagTypes: ['Auth', 'User', 'Chat', 'Message', 'Consultation', 'ContactLead','Categories','Products','DoctorDashboard','Consultations','Patient', 
+    'Dashboard','Orders','Cart','HomepageContent','WebsiteSettings','Notifications','Doctors', 'Testimonials', 'Discounts', 'HeroSections', 'CtaSections', 'CoverageSection', 'Faq', 'Eligibility'],
   endpoints: () => ({}),
 })
 
